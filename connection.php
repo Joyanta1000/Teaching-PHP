d739f2de1772ab2948f8dd0187da34a106437299
Joyanta Dutta <58521830+Joyanta1000@users.noreply.github.com>
2021-07-03 00:36:57 +0600

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "tractis";
+$servername = "ec2-52-0-67-144.compute-1.amazonaws.com";
+$username = "wbqdjmodukqqmv";
+$password = "18a9405accc2b07be5307f6872bb71dde4bf59a2f511cf6b9f0bc5a0627ee71a";
+$dbname = "d4koueh8ic01rb";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
